96efa73cf88b20631fd71dae7e849f1a05017d1d
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-01-01 13:00:16 -0500

    fixed moving background, and made navbar functional

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/nav';
-import Home from './components/home/home'
-import Binary from './components/moving/moving'
+import Home from './components/home/home';
+import About from './components/about/about';
+import Skills from './components/skills/skills';
+import Binary from './components/moving/moving';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Binary />
       <Home />
-
+      <About />
+      <Skills />
       <Navbar />
     </div>
   );
